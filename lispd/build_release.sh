f4419a8a37ecf69107307439676fec62dee18331
chris <chris@8d6a36a1-ccbd-4c22-bd2f-22b22082b15b>
2011-05-26 17:33:10 +0000

    o Update installer scripts for v6. Include new busybox.

--- lispd/build_release.sh
+++ lispd/build_release.sh
@@ -3,9 +3,11 @@ rm -rf releasedir
 mkdir releasedir
 cp ../../out/target/product/passion/obj/EXECUTABLES/lispd_intermediates/LINKED/lispd releasedir
 cp lispd.conf releasedir
-cp ../lisp_mod/lisp.ko releasedir
+cp lispdv6.conf releasedir
+cp ../lisp_mod6/lisp.ko releasedir
 cp ../../out/target/product/passion/obj/EXECUTABLES/lispconf_intermediates/LINKED/lispconf releasedir
 cp ../../out/target/product/passion/obj/EXECUTABLES/lig_intermediates/LINKED/lig releasedir
 cp ../../out/target/product/passion/obj/EXECUTABLES/lispmanager_intermediates/LINKED/lispmanager releasedir
 cp ~chris/workspace/lispmonApp/bin/lispmonApp.apk releasedir
 cp release_install.sh releasedir/install.sh
+cp ../busybox-1.18.4/busybox releasedir
--- lispd/release_install.sh
+++ lispd/release_install.sh
@@ -7,5 +7,7 @@ adb push ./lisp.ko /system/lib/modules/lisp.ko
 adb push ./lispconf /system/bin/lispconf
 adb push ./lig /system/bin/lig
 adb push ./lispmanager /system/bin/lispmanager
+adb push ./busybox /system/bin/busybox
 adb shell chmod 4755 /system/bin/lispmanager
+adb shell chmod 4755 /system/bin/busybox
 adb install -r ./lispmonApp.apk
